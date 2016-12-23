b2e78409c3f8fae6f03abd765160a3c78bb492e7
john1625b <john1625b@gmail.com>
2016-12-23 01:07:22 -0500

    adds listner for button click

--- app/src/main/java/com/example/john/arewethereyet/MainActivity.java
+++ app/src/main/java/com/example/john/arewethereyet/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.john.arewethereyet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.john.arewethereyet.R.id.textBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button but1 = (Button) findViewById(R.id.butt1);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                but1.setText("button clicked");
+            }
+        });
     }
 }
