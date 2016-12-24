d40a79e95efa6adbd392b9efda39ce41213e852a
John Fernandes <john1625b@gmail.com>
2016-12-24 16:05:28 -0500

    adds volley

--- app/src/main/java/com/example/john/arewethereyet/MainActivity.java
+++ app/src/main/java/com/example/john/arewethereyet/MainActivity.java
@@ -3,9 +3,8 @@ package com.example.john.arewethereyet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import static com.example.john.arewethereyet.R.id.textBox;
+import android.widget.Button; // import general button class
+import android.widget.TextView; // import general textView class
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
                 buttonClickCount++;
             }
         });
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
