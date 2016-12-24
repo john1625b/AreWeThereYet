ff2d0dddc622d55a6c6e2104f7027bd9528f9766
John Fernandes <john1625b@gmail.com>
2016-12-24 17:46:51 -0500

    adds import statements for volley

--- app/src/main/java/com/example/john/arewethereyet/MainActivity.java
+++ app/src/main/java/com/example/john/arewethereyet/MainActivity.java
@@ -6,6 +6,13 @@ import android.view.View;
 import android.widget.Button; // import general button class
 import android.widget.TextView; // import general textView class
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
