7f67bbafd2f7c4392c63a7073606107544a3cc72
john1625b <john1625b@gmail.com>
2016-12-26 03:08:53 -0500

    adds working destination and curr location input fields

--- app/src/main/java/com/example/john/arewethereyet/MainActivity.java
+++ app/src/main/java/com/example/john/arewethereyet/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button; // import general button class
+import android.widget.EditText;
 import android.widget.TextView; // import general textView class
 
 import com.android.volley.Request; // import volley http requester library
@@ -24,54 +25,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button but1 = (Button) findViewById(R.id.butt1);
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+        final EditText currentLocation = (EditText) findViewById(R.id.currentLocation);
+        final EditText desinationLocation = (EditText) findViewById(R.id.destinationLocation);
+
+// Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
         but1.setOnClickListener(new View.OnClickListener() {
             int buttonClickCount = 0;
             @Override
             public void onClick(View v) {
-                but1.setText("button clicked " + buttonClickCount + " times");
-                buttonClickCount++;
-            }
-        });
-
-        final TextView mTextView = (TextView) findViewById(R.id.text);
-
-// Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=Nwe+Yrk+City,NY&key=AIzaSyDlhkW8SbeMnqWvenKzsP0_A2-SpqQg7hY";
+//                but1.setText("button clicked " + buttonClickCount + " times");
+//                buttonClickCount++;
+                final String currLocationString = currentLocation.getText().toString();
+                final String destLocationString = desinationLocation.getText().toString();
+                String url ="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + currLocationString +
+                        "&destinations="+ destLocationString + "&key=AIzaSyDlhkW8SbeMnqWvenKzsP0_A2-SpqQg7hY";
 
 // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
+                final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
 //                        mTextView.setText("Response is: "+ response.substring(0,500));
-                        try {
-                            JSONObject reader = new JSONObject(response);
+                                try {
+                                    JSONObject reader = new JSONObject(response);
 //                            String rowsString = reader.getString("rows");
 //                            JSONObject rowsObject = new JSONObject(rowsString);
-                            JSONArray rows = reader.getJSONArray("rows");
-                            JSONObject row0 = rows.getJSONObject(0);
-                            JSONArray elements = row0.getJSONArray("elements");
-                            JSONObject elements0 = elements.getJSONObject(0);
-                            JSONObject duration = elements0.getJSONObject("duration");
-                            String value = duration.getString("text");
-                            mTextView.setText(value);
-//                            mTextView.setText("Response is: "+ rows);
-                        } catch (JSONException e) {
-                            mTextView.setText("reacher catch block");
-                            e.printStackTrace();
-                        }
+                                    JSONArray rows = reader.getJSONArray("rows");
+                                    JSONObject row0 = rows.getJSONObject(0);
+                                    JSONArray elements = row0.getJSONArray("elements");
+                                    JSONObject elements0 = elements.getJSONObject(0);
+                                    JSONObject duration = elements0.getJSONObject("duration");
+                                    String value = duration.getString("text");
+                                    mTextView.setText(value);
+                                } catch (JSONException e) {
+                                    mTextView.setText("couldn't parse responseJSON");
+                                    e.printStackTrace();
+                                }
 
 
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("volley request error");
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
+                });
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+
     }
 }
